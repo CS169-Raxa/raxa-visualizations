504ae92957429ca64e1332210dec7ae68ffed853
Eric Tzeng <eric.s.tzeng@gmail.com>
2012-10-12 01:37:14 -0700

    Initial generation of rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaxaVisualizations::Application.config.secret_token = '37cffd6ad6b9b643ca42b195f3023756f14b48a46a6756be12f92f91cb28c7704dae2d4613142d5761e686a1f0fa9ba78860240d9414498d201429ed554a85c2'
