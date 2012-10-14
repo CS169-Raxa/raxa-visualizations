d53f7e3f6fccbde486ede70357b3b2d6c5e41a59
bucephalus <saasbook@saasbook.(none)>
2012-10-14 13:51:26 -0700

    added drug_steps.rb to new folder features/step_definitions with all pending step definitions suggested by cucumber

---
+++ features/step_definitions/drug_steps.rb
@@ -0,0 +1,44 @@
+Given /^the following drugs exist:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I manually set the low_stock_point for "(.*?)" to (\d+)$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the low_stock_pint for "(.*?)" should be (\d+)$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I automatically set the low_stock_point for "(.*?)" to (\d+)$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I am on the pharmacy dashboard$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see an alert for "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should not see an alert for "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the quantity of "(.*?)" is set to (\d+)$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the low_stock_alert for "(.*?)" should be False$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the low_stock_alert for "(.*?)" should be True$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the low_stock_point of "(.*?)" is set to (\d+)$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
