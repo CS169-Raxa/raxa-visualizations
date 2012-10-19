ff01798a92857af8e53952ffa8d41d682d5b70cf
Eric Tzeng <eric.s.tzeng@gmail.com>
2012-10-19 01:20:01 -0700

    Barebones specs for PharmacyController#index and its view.

--- spec/controllers/pharmacy_controller_spec.rb
+++ spec/controllers/pharmacy_controller_spec.rb
@@ -7,6 +7,17 @@ describe PharmacyController do
       get 'index'
       response.should be_success
     end
+
+    it "assigns @drugs" do
+      drug = Drug.create
+      get :index
+      assigns(:drugs).should eq([drug])
+    end
+
+    it "renders the index template" do
+      get :index
+      response.should render_template("index")
+    end
   end
 
 end
