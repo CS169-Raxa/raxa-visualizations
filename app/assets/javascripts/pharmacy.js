dcdd6f6771752e33c1f68780e7e5c0caa3d07d5a
Eric Tzeng <eric.s.tzeng@gmail.com>
2012-10-19 01:08:06 -0700

    Javascript to make Pharmacy view rows expand.

--- app/assets/javascripts/pharmacy.js
+++ app/assets/javascripts/pharmacy.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $('.details div').hide();
+  $('.info').bind('click', function() {
+      $(this).next().find('div').slideToggle();
+  });
+});
