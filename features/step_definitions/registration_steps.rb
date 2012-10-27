350c62d6d53f84c357d2c9b427247a6944a31b17
Christina Wong <cawong@berkeley.edu>
2012-10-27 10:32:12 -0700

    added step definitions

---
+++ features/step_definitions/registration_steps.rb
@@ -0,0 +1,29 @@
+# general
+Given /^the following patients exist:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the following registrars exist:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the following registrations exist:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+# paths
+Given /^I am on the (.*?) registration dashboard$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+# avg time to register a patient
+Then /^I should see that the average time to register a patient is (\d+) minutes$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^(.*?) registers a patient from (\d+)\-(\d+)\-(\d+) (\d+):(\d+) to (\d+)\-(\d+)\-(\d+) (\d+):(\d+)$/ do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10|
+  pending # express the regexp above with the code you wish you had
+end
