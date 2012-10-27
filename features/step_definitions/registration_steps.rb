33e97b275e3888f3fd77387049c2b8d4a342c1ff
Christina Wong <cawong@berkeley.edu>
2012-10-26 20:31:43 -0700

    added registration_steps.rb
    last commit didn't add it, I guess

---
+++ features/step_definitions/registration_steps.rb
@@ -0,0 +1,23 @@
+# general
+Given /^that (\d+) patients have been registered today$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+
+# paths
+Given /^I am on the registration dashboard$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+# num patients registered
+Then /^I should see (\d+) as the number of patients registered$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I register a patient$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^it is the next day$/ do
+  pending # express the regexp above with the code you wish you had
+end
