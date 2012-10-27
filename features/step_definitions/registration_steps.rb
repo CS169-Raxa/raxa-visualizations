dd23a0d3c0150cf7bd6dfb1bbd9da3eac4634a68
Christina Wong <cawong@berkeley.edu>
2012-10-27 11:38:57 -0700

    removed obsolete step definition for setting current date

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -26,10 +26,6 @@ When /^(.*?) registers a patient$/ do
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^the date is (\d+)\-(\d+)\-(\d+)$/ do |arg1, arg2, arg3|
-  pending # express the regexp above with the code you wish you had
-end
-
 When /^I register a patient$/ do
   pending # express the regexp above with the code you wish you had
 end
