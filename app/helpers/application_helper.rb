e8d6a66b86fff4fb48b229ec30f5d7d87fa93f20
Seshadri Mahalingam <seshadri@berkeley.edu>
2012-10-28 03:35:38 -0700

    [helper] make JSON dates return in JS-style UNIX time in ms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  class ActiveSupport::TimeWithZone
+    # The default rails as_json method format isn't understood by the
+    # browser. Use UNIX epoch time (in milliseconds) instead.
+    #
+    # Client-side JS should use `new Date(...)`
+    def as_json(options = {})
+      to_i * 1000.0
+    end
+  end
 end
