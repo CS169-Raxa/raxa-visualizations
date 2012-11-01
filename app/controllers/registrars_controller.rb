3e5189b443f427fe71f58376ab1883c9dcfbf585
Eric Tzeng <eric.s.tzeng@gmail.com>
2012-10-31 19:30:05 -0700

    Bug fix: Time.now works better than Date.today because of time zone shenanigans.

--- app/controllers/registrars_controller.rb
+++ app/controllers/registrars_controller.rb
@@ -1,10 +1,10 @@
 class RegistrarsController < ApplicationController
   def index
-    @num_today = Registration.for_day(Date.today).count
+    @num_today = Registration.for_day(Time.now).count
     render :show
   end
   def show
     @registrar = Registrar.find(params[:id])
-    @num_today = @registrar.registrations_for_day(Date.today).count
+    @num_today = @registrar.registrations_for_day(Time.now).count
   end
 end
