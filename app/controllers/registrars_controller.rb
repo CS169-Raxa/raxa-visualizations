f9649200f4ef4e6645452848628d254407bf2beb
Dan Wang <d.wang1991@gmail.com>
2012-11-02 01:05:06 -0700

    Pass in vars instead of the registration object

--- app/controllers/registrars_controller.rb
+++ app/controllers/registrars_controller.rb
@@ -1,10 +1,18 @@
 class RegistrarsController < ApplicationController
   def index
     @num_today = Registration.for_day(Time.now).count
+    @average_time = average_time(Registration.all)
     render :show
   end
   def show
-    @registrar = Registrar.find(params[:id])
-    @num_today = @registrar.registrations_for_day(Time.now).count
+    registrar = Registrar.find(params[:id])
+    @num_today = registrar.registrations_for_day(Time.now).count
+    @average_time = average_time(registrar.registrations)
+  end
+
+  protected
+  def average_time(registrations)
+    total_time = registrations.map {|r| r.elapsed_time}.reduce(:+)
+    return ChronicDuration.output((total_time/registrations.length).to_i, :format => :chrono)
   end
 end
