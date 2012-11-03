7faf7dbf21efb6189ba6e46a0c8b37557465825d
Christina Wong <cawong@berkeley.edu>
2012-11-02 17:21:56 -0700

    removed patient_name and registrar_name from registration model

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,18 +1,8 @@
 class Registration < ActiveRecord::Base
-  attr_accessible :patient_status, :time_end, :time_start, :patient_name, :registrar_name
+  attr_accessible :patient_status, :time_end, :time_start
   validates :patient_status, :inclusion => {:in => ['new', 'returning']}
   belongs_to :patient
   belongs_to :registrar
-
-  def patient_name= (name)
-      self.patient = Patient.find_by_name(name)
-      @patient_name = name
-  end
-
-  def registrar_name= (name)
-      self.registrar = Registrar.find_by_name(name)
-      @registrar_name = name
-  end
 
   scope :for_day, lambda { |time|
     day = time.beginning_of_day
