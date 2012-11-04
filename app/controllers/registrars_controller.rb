59cd32c8ba06fc2656d6e8820dd17ae32efafcfe
Christina Wong <cawong@berkeley.edu>
2012-11-03 20:50:13 -0700

    set up @registrations_and_divs in index of registrars_controller.rb

--- app/controllers/registrars_controller.rb
+++ app/controllers/registrars_controller.rb
@@ -2,6 +2,30 @@ class RegistrarsController < ApplicationController
   def index
     @num_today = Registration.for_day(Time.now).count
     @average_time = average_time(Registration.all)
+
+    # set up @registrations_and_divs for registrations table in view
+    # @registrations_and_divs is a list of
+    #    two-element-lists where the first element is a registration
+    #    and the second element indicates whether or not to place a divider
+    #        before the next registration in the table
+    #        using false for no divider, and the divider text otherwise
+    @registrations = Registration.all(:order => "time_end")
+    @registrations_and_divs = []
+    last_reg = Chronic.parse('today').localtime.strftime('%^B %e, %Y')
+    @registrations.reverse.each do |reg|
+      div = false
+      reg_date = reg.time_end.localtime.strftime('%^B %e, %Y')
+      if last_reg != reg_date
+        if Chronic.parse('yesterday').localtime.strftime('%^B %e, %Y') == reg_date
+          div = 'YESTERDAY'
+        else
+          div = reg_date
+        end
+      end
+      last_reg = reg_date
+      @registrations_and_divs << [reg, div]
+    end
+
     render :show
   end
 
@@ -16,11 +40,7 @@ class RegistrarsController < ApplicationController
     #    and the second element indicates whether or not to place a divider
     #        before the next registration in the table
     #        using false for no divider, and the divider text otherwise
-    if params[:id]
-      @registrations = Registration.all(:order => "time_end", :conditions => {:registrar_id => params[:id]})
-    else
-      @registrations = Registration.all(:order => "time_end")
-    end
+    @registrations = Registration.all(:order => "time_end", :conditions => {:registrar_id => params[:id]})
     @registrations_and_divs = []
     last_reg = Chronic.parse('today').localtime.strftime('%^B %e, %Y')
     @registrations.reverse.each do |reg|
