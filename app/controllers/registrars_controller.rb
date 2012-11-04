fb990bd93acb664f3321f3b0822fc43022a50b80
Christina Wong <cawong@berkeley.edu>
2012-11-04 14:41:34 -0800

    changed representation of @registrations_and_divs in registars_controller.rb

--- app/controllers/registrars_controller.rb
+++ app/controllers/registrars_controller.rb
@@ -30,27 +30,28 @@ class RegistrarsController < ApplicationController
   def get_regs_by_date(registrations)
     # set up @registrations_and_divs for registrations table in view
     # @registrations_and_divs is a list of
-    #    two-element-lists where the first element is a registration
-    #    and the second element indicates whether or not to place a divider
-    #        before the next registration in the table
-    #        using false for no divider, and the divider text otherwise
-    regs_and_divs = []
+    #    hash tables where the keys are
+    #      :date => a string to show in the dividers in the view (nil if today)
+    #      :regs => registrations that took place on that date
+    curr_regs = {}
+    regs_and_divs = [curr_regs]
     last_reg = Chronic.parse('today').localtime.strftime('%^B %e, %Y')
     registrations.each do |reg|
-      div = false
       reg_date = reg.time_end.localtime.strftime('%^B %e, %Y')
       if last_reg != reg_date
+        curr_regs = {}
+        regs_and_divs << curr_regs
         if Chronic.parse('yesterday').localtime.strftime('%^B %e, %Y') == reg_date
-          div = 'YESTERDAY'
+          curr_regs[:date] = 'YESTERDAY'
         else
-          div = reg_date
+          curr_regs[:date] = reg_date
         end
+        last_reg = reg_date
       end
-      last_reg = reg_date
-      regs_and_divs << [reg, div]
+      curr_regs[:regs] << reg
     end
-    if regs_and_divs == []
-      regs_and_divs << [nil, "there are no registrations to show"]
+    if registrations.empty?
+      regs_and_divs = [{:date = "there are no registrations to show"}]
     end
     return regs_and_divs
   end
