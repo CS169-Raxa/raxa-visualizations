929d5a8816bde2b685663f1d89510d7cceedf2f1
Dan Wang <d.wang1991@gmail.com>
2012-11-19 05:10:19 -0800

    Mock view for screener

--- app/assets/javascripts/screener.js
+++ app/assets/javascripts/screener.js
@@ -1,2 +1,22 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var Screener = function() { };
+
+Screener.prototype = new Raxa();
+
+Screener.prototype.init = function() {
+  Raxa.prototype.init.call(this);
+  this.colourBars();
+};
+
+Screener.prototype.colourBars = function() {
+  var color = d3.scale.linear()
+    .domain([0, 50, 100])
+    .range(["#116011", "#dd9022", "#bb4444"]);
+  $('.bar').each(function(i, e) {
+    e.style.backgroundColor = color($(e).data('percent'));
+  });
+};
+
+$(document).ready(function() {
+  screener = new Screener();
+  screener.init();
+});
