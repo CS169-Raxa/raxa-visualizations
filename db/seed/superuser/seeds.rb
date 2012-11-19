3bbdc4196fa3f136b38e80026576201712696809
Dan Wang <d.wang1991@gmail.com>
2012-11-18 18:03:09 -0800

    Seed for encoutners

--- db/seed/superuser/seeds.rb
+++ db/seed/superuser/seeds.rb
@@ -6,8 +6,18 @@ Dir.glob(data_path.join('*.yaml')) do |path|
   patient_data, encounters_data = YAML.load_stream(File.open(path))
   patient = Patient.create(patient_data)
   encounters_data.each do |ed| 
-    ed['timestamp'] = Chronic::parse(ed['timestamp'])
-    encounter = Encounter.create(ed)
+    start_time = Chronic::parse(ed['start_time'])
+    end_time = Chronic::parse(ed['end_time'])
+
+    department = Department.find_by_name(ed['department']) ||
+      Department.create(:name => ed['department'])
+
+    encounter = Encounter.create(
+      :start_time => start_time,
+      :end_time => end_time
+    )
+
+    department.encounters << encounter
     patient.encounters << encounter
   end
   patient.save
