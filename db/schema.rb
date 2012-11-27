712f46a0c497b721cf868d3f8a1e6136f0850e10
Christina Wong <cawong@berkeley.edu>
2012-11-26 18:38:23 -0800

    migration for association (doctor/patient)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126233500) do
+ActiveRecord::Schema.define(:version => 20121126235209) do
 
   create_table "departments", :force => true do |t|
     t.string   "name"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20121126233500) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "doctor_id"
   end
 
   create_table "registrars", :force => true do |t|
