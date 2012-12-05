abb8d9181db3ea0104aaad135c70015efd47b90e
Dan Wang <d.wang1991@gmail.com>
2012-12-05 02:02:36 -0800

    fixing bad sort

--- app/controllers/pharmacy_controller.rb
+++ app/controllers/pharmacy_controller.rb
@@ -1,7 +1,8 @@
 class PharmacyController < ApplicationController
   # see how long drug will last based on previous usage rates
   def index
-    @drugs = Drug.all.sort_by{|drug| drug.time_left}
-    @drugs = @drugs.sort_by{|drug| drug.alert? ? 0 : 1}
+    @drugs = Drug.all.sort_by do |drug|
+      [drug.time_left, drug.alert? ? 0 : 1]
+    end
   end
 end
