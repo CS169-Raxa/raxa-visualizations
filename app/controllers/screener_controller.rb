1783cce368e4467e1b9a33c62981098138bf3132
Eric Tzeng <eric.s.tzeng@gmail.com>
2012-12-04 19:46:17 -0800

    Hooked in controller logic for screener.

--- app/controllers/screener_controller.rb
+++ app/controllers/screener_controller.rb
@@ -1,4 +1,8 @@
 class ScreenerController < ApplicationController
   def index
+    @sorted_doctors = Doctor.sorted_by_num_patients
+    @max_patients = [@sorted_doctors.last.patients.size, 1].max
+    @patients_left = 7
+    @specialties = Specialty.all
   end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,7 +5,17 @@ class Doctor < ActiveRecord::Base
   has_many :patients
   has_and_belongs_to_many :specialties
 
+  scope :sorted_by_num_patients, (lambda do
+    select("#{Doctor.table_name}.*, COUNT(#{Patient.table_name}.id) num_pats")
+      .joins(Patient.table_name.to_sym)
+      .order(:num_pats)
+  end)
+
   def num_patients
     return self.patients.size
+  end
+
+  def specialty_names
+    self.specialties.map{|specialty| specialty.name}
   end
 end
