3eea6d5d644ce495d565fb959c88fde070b8217a
Eric Tzeng <eric.s.tzeng@gmail.com>
2012-12-05 00:36:53 -0800

    Fixed sorting for both pharmacy and screener.

--- app/controllers/pharmacy_controller.rb
+++ app/controllers/pharmacy_controller.rb
@@ -1,6 +1,7 @@
 class PharmacyController < ApplicationController
   # see how long drug will last based on previous usage rates
   def index
-    @drugs = Drug.all
+    @drugs = Drug.all.sort_by{|drug| drug.time_left}
+    @drugs = @drugs.sort_by{|drug| drug.alert? ? 0 : 1}
   end
 end
