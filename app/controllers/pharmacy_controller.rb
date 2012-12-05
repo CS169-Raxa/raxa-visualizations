460332fe2e6e48253ed34898908d47c5a6f0d6f5
Eric Tzeng <eric.s.tzeng@gmail.com>
2012-12-05 02:20:15 -0800

    Fixed a bug in the sorting (again) that was caused by time_left being false.

--- app/controllers/pharmacy_controller.rb
+++ app/controllers/pharmacy_controller.rb
@@ -1,8 +1,10 @@
 class PharmacyController < ApplicationController
   # see how long drug will last based on previous usage rates
   def index
-    @drugs = Drug.all.sort_by do |drug|
+    has_history, no_history = Drug.all.partition{|drug| drug.time_left != false}
+    has_history.sort_by! do |drug|
       [drug.time_left, drug.alert? ? 0 : 1]
     end
+    @drugs = no_history + has_history
   end
 end
