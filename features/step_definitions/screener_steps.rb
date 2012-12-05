6ef29117917213690ee438829c9e389137b6e455
Dan Wang <d.wang1991@gmail.com>
2012-12-05 01:59:57 -0800

    Last feature step definitions

--- features/step_definitions/screener_steps.rb
+++ features/step_definitions/screener_steps.rb
@@ -12,7 +12,9 @@ Given /^the following doctors exist:$/ do |table|
       .map {|s| Specialty.find_by_name(s) || Specialty.create(:name => s)}
 
     doctor['number of patients'].to_i.times do
-      d.patients << Patient.create
+      d.patients << Patient.create!(
+        :name => 'YY_PATIENT'
+      )
     end
   end
 end
@@ -33,11 +35,8 @@ end
 When /^I should (not )?see the following doctors: (.*)/ do |negate, doctors|
   doctors.split(/,\s+/).each do |doctor|
     query = have_css '.doctor_name', :text => doctor
-    if negate
-      page.should_not query
-    else
-      page.should query
-    end
+    expectation = negate ? :should_not : :should
+    page.send(expectation, query)
   end
 end
 
@@ -49,6 +48,22 @@ When /^Patients (.*?)\-(.*?) have been assigned$/ do |let1, let2|
   visit(current_path)
 end
 
-Then /^I should see (\d+) patient(s?) to assign$/ do |num, _|
+Then /^I should see (\d+) patients? to assign$/ do |num|
   find('#patients_left').should have_content num
+end
+
+Then /^(.*) should have (\d+) patients? assigned$/ do |name, patients|
+  doctor = Doctor.find_by_name(name)
+  find("#doctor-#{doctor.id} .bar-text").should have_content "#{patients} / #{doctor.max_workload}"
+end
+
+Then /^I should see doctor (.*) at (\d+)% workload$/ do |name, workload|
+  doctor = Doctor.find_by_name(name)
+  width = /width:\s*(\d+)\s*%/
+    .match(find("#doctor-#{doctor.id} .bar")[:style])[1]
+  (width.to_f - workload.to_f).abs.should <= 1
+end
+
+Then /^I should see doctor (.*) before doctor (.*)$/ do |name1, name2|
+  page.body.should =~ /#{name1}.*#{name2}/
 end
